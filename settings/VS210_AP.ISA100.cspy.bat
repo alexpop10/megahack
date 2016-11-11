dd0dbcb645ab235381b81857f3797d09266da474
apphx <alex.pop1819@gmail.com>
2016-11-11 18:34:07 +0200

    updated project version

--- settings/VS210_AP.ISA100.cspy.bat
+++ settings/VS210_AP.ISA100.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\Projects\TechFest 2016\ISA100-Application-Processor - Lib\settings\VS210_AP.ISA100.general.xcl" --backend -f "D:\Projects\TechFest 2016\ISA100-Application-Processor - Lib\settings\VS210_AP.ISA100.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "D:\facultate\megahack\code\ISA100-Application-Processor-master\settings\VS210_AP.ISA100.general.xcl" --backend -f "D:\facultate\megahack\code\ISA100-Application-Processor-master\settings\VS210_AP.ISA100.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\Projects\TechFest 2016\ISA100-Application-Processor - Lib\settings\VS210_AP.ISA100.general.xcl" "--debug_file=%~1" --backend -f "D:\Projects\TechFest 2016\ISA100-Application-Processor - Lib\settings\VS210_AP.ISA100.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "D:\facultate\megahack\code\ISA100-Application-Processor-master\settings\VS210_AP.ISA100.general.xcl" "--debug_file=%~1" --backend -f "D:\facultate\megahack\code\ISA100-Application-Processor-master\settings\VS210_AP.ISA100.driver.xcl" 
 
 @echo off 
 :end
